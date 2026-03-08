80daad777e38ccaf4657199f56155c7c4306974b
zach-west <west.zachariah@gmail.com>
2026-03-08 19:52:06 +1100

    Update capacitor.config.ts
    Removed the external link URL to stop looking for an external website (like the Lovable preview) and instead load the local code bundled inside the APK. So the app will no longer try to "handshake" with Lovable or open your mobile browser

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,10 +4,6 @@ const config: CapacitorConfig = {
   appId: 'app.lovable.f6dbc7a6d25e4bc9b63836040c1c8e31',
   appName: 'mono-brass-pulse',
   webDir: 'dist',
-  server: {
-    url: 'https://f6dbc7a6-d25e-4bc9-b638-36040c1c8e31.lovableproject.com?forceHideBadge=true',
-    cleartext: true,
-  },
   android: {
     allowMixedContent: true,
   },
